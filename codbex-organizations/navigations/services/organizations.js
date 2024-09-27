58f94cbbe539242c571b860d993cd7c71539a776
TIVMOF <tomyivanov06@gmail.com>
2024-09-27 14:00:17 +0000

    fix: Correct group

--- codbex-organizations/navigations/services/organizations.js
+++ codbex-organizations/navigations/services/organizations.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'organizations-navigation',
     label: "Organizations",
     view: "organizations",
-    group: "organizations",
+    group: "employees",
     orderNumber: 1000,
     lazyLoad: true,
     link: "/services/web/codbex-organizations/gen/codbex-organizations/ui/Organizations/index.html?embedded"
