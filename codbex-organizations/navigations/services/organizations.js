0260d21b7496af3ec1f5c0537f68ea04e1fcbdfe
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2024-10-01 17:49:58 +0300

    Added navigations (#3)
    * feat: Added navigations
    
    * fix: Correct group

---
+++ codbex-organizations/navigations/services/organizations.js
@@ -0,0 +1,19 @@
+const navigationData = {
+    id: 'organizations-navigation',
+    label: "Organizations",
+    view: "organizations",
+    group: "employees",
+    orderNumber: 1000,
+    lazyLoad: true,
+    link: "/services/web/codbex-organizations/gen/codbex-organizations/ui/Organizations/index.html?embedded"
+};
+
+function getNavigation() {
+    return navigationData;
+}
+
+if (typeof exports !== 'undefined') {
+    exports.getNavigation = getNavigation;
+}
+
+export { getNavigation }
