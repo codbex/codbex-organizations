781b1d717e40ce9cb05c39d669416679e856278d
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-03-28 11:18:09 +0200

    Update service.js

--- codbex-organizations/navigations/organizations/service.js
+++ codbex-organizations/navigations/organizations/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'organizations-navigation',
     label: "Organizations",
     group: "company",
-    order: 100,
+    order: 200,
     link: "/services/web/codbex-organizations/gen/codbex-organizations/ui/Organizations/index.html?embedded"
 };
 
